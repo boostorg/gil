105a96d354539b18d22da313f824a0d6f9563dfd
Lubomir Bourdev <lbourdev@adobe.com>
2010-05-11 01:07:00 +0000

    GIL: Added support for copying between variants of different types
    [SVN r61899]

--- include/boost/gil/extension/dynamic_image/any_image.hpp
+++ include/boost/gil/extension/dynamic_image/any_image.hpp
@@ -84,9 +84,11 @@ public:
     template <typename T> explicit any_image(const T& obj)               : parent_t(obj) {}
     template <typename T> explicit any_image(T& obj, bool do_swap)       : parent_t(obj,do_swap) {}
     any_image(const any_image& v)                                        : parent_t((const parent_t&)v)    {}
+    template <typename Types> any_image(const any_image<Types>& v)       : parent_t((const variant<Types>&)v)    {}
 
-    template <typename T> any_image& operator=(const T& obj)             { parent_t::operator=(obj); return *this; }
-    any_image&                       operator=(const any_image& v)       { parent_t::operator=((const parent_t&)v); return *this;}
+    template <typename T> any_image& operator=(const T& obj)                  { parent_t::operator=(obj); return *this; }
+    any_image&                       operator=(const any_image& v)            { parent_t::operator=((const parent_t&)v); return *this;}
+	template <typename Types> any_image& operator=(const any_image<Types>& v) { parent_t::operator=((const variant<Types>&)v); return *this;}
 
     void recreate(const point_t& dims, unsigned alignment=1)               { apply_operation(*this,detail::recreate_image_fnobj(dims,alignment)); }
     void recreate(x_coord_t width, y_coord_t height, unsigned alignment=1) { recreate(point2<std::ptrdiff_t>(width,height),alignment); }
