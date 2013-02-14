75342fedd2c5b6c44f550cbf9a4ca48d187a2f75
Christian Henning <chhenning@gmail.com>
2013-02-14 16:00:09 +0000

    Added more specializations for get_num_bits.
    [SVN r82882]

--- toolbox/test/get_pixel_type.cpp
+++ toolbox/test/get_pixel_type.cpp
@@ -15,14 +15,16 @@ BOOST_AUTO_TEST_CASE( get_pixel_type_test )
     {
         typedef bit_aligned_image3_type< 4, 15, 4, rgb_layout_t >::type image_t;
         BOOST_STATIC_ASSERT(( is_same< get_pixel_type< image_t::view_t >::type
-                            , image_t::view_t::reference >::value
+                                     , image_t::view_t::reference 
+                                     >::value
                            ));
     }
 
     {
         typedef rgb8_image_t image_t;
         BOOST_STATIC_ASSERT(( is_same< get_pixel_type< image_t::view_t >::type
-                            , image_t::view_t::value_type >::value
+                                     , image_t::view_t::value_type
+                                     >::value
                            ));
     }
 }
