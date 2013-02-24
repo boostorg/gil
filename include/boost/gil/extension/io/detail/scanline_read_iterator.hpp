4bcc9c531053afc89bf52b2c4d3280dd3648c6d8
Christian Henning <chhenning@gmail.com>
2013-02-24 21:06:54 +0000

    Using iterator_facade as base for scanline_read_iterator.
    [SVN r83141]

--- include/boost/gil/extension/io/detail/scanline_read_iterator.hpp
+++ include/boost/gil/extension/io/detail/scanline_read_iterator.hpp
@@ -13,13 +13,15 @@
 ////////////////////////////////////////////////////////////////////////////////////////
 /// \file
 /// \brief
-/// \author Christian Henning \n
+/// \author Christian Henning
 ///
 /// \date 2012 \n
 ///
 ////////////////////////////////////////////////////////////////////////////////////////
 
-#include <vector>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/iterator/iterator_facade.hpp>
 
 #include <boost/gil/extension/io/detail/io_error.hpp>
 
@@ -32,34 +34,49 @@ namespace boost { namespace gil {
 
 /// Input iterator to read images.
 template< typename Reader >
-class scanline_read_iterator
+class scanline_read_iterator : public boost::iterator_facade< scanline_read_iterator< Reader >
+                                                            , byte_t*
+                                                            , std::input_iterator_tag
+                                                            >
 {
 public:
 
-    typedef typename Reader::backend_t backend_t;
-
-    typedef std::input_iterator_tag iterator_category;
-    typedef byte_t* value_type;
-    typedef value_type const* pointer;
-    typedef value_type const& reference;
-    typedef int difference_type;
-    
-public:
-
-    /// Constructor with preallocated image. Reading starts at first scanline of source image.
     scanline_read_iterator( Reader& reader
-                          , int pos = 0
+                          , int     pos = 0
                           )
     : _pos( pos )
     , _read_scanline( true )
     , _skip_scanline( true )
     , _reader( reader )
     {
-        init();
+        _buffer = boost::make_shared<  buffer_t >( buffer_t( _reader._scanline_length ));
+        _buffer_start = &_buffer->front();
     }
 
-    /// Dereference Operator
-    reference operator*()
+private:
+    friend class boost::iterator_core_access;
+
+    void increment()
+    {
+        if( _skip_scanline == true )
+        {
+            _reader.skip( _buffer_start
+                        , _pos
+                        );
+        }
+
+        ++_pos;
+
+        _skip_scanline = true;
+        _read_scanline = true;
+    }
+
+    bool equal( const scanline_read_iterator& rhs ) const
+    {
+        return _pos == rhs._pos;
+    }
+
+    reference dereference() const
     {
         if( _read_scanline == true )
         {
@@ -74,64 +91,19 @@ public:
         return _buffer_start;
     }
 
-    /// Pointer-To-Memper Operator.
-    pointer operator->() const
-    {
-        return &(operator*());
-    }
-
-    /// Pre-Increment Operator
-    scanline_read_iterator< Reader >& operator++()
-    {
-        if( _skip_scanline == true )
-        {
-            _skip();
-        }
-
-        ++_pos;
-
-        _skip_scanline = true;
-        _read_scanline = true;
-
-        return (*this);
-    }
-
-    bool operator ==( const scanline_read_iterator< Reader >& rhs ) const
-    {
-        return _pos == rhs._pos;
-    }
-
-    bool operator !=( const scanline_read_iterator< Reader >& rhs ) const
-    {
-        return _pos < rhs._pos;
-    }
-
 private:
-
-    void init()
-    {
-        _buffer = std::vector< byte_t >( _reader._scanline_length );
-        _buffer_start = &_buffer.front();
-    }
-
-    void _skip()
-    {
-        _reader.skip( _buffer_start
-                    , _pos
-                    );
-    }
-
-private:
+    Reader& _reader;
 
     mutable int _pos;
 
     mutable bool _read_scanline;
     mutable bool _skip_scanline;
 
-    Reader& _reader;
+    typedef std::vector< byte_t > buffer_t;
+    typedef boost::shared_ptr< buffer_t > buffer_ptr_t;
 
-    std::vector< byte_t > _buffer;
-    byte_t*               _buffer_start;
+    buffer_ptr_t _buffer;
+    mutable byte_t* _buffer_start;
 };
 
 #if BOOST_WORKAROUND(BOOST_MSVC, >= 1400) 
