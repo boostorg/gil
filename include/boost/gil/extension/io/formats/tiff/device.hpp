037de1f3093910949d48b20b868ca2f6c89d171b
William Gallafent <william@gallaf.net>
2014-05-31 13:00:10 +0100

    Minor header / indent correction

--- include/boost/gil/extension/io/formats/tiff/device.hpp
+++ include/boost/gil/extension/io/formats/tiff/device.hpp
@@ -35,7 +35,7 @@
 
 #include <tiffio.hxx>
 
-#include <boost/fusion/sequence/intrinsic/size.hpp>
+#include <boost/mpl/size.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/utility/enable_if.hpp>
 
@@ -137,12 +137,11 @@ public:
     : _tiff_file( tiff_file
                 , TIFFClose )
     {}
-
-	template <typename Property>
-	bool get_property( typename Property::type& value  )
-	{
-		return get_property_f <mpl:: size <typename Property:: arg_types>::value > (). template operator () <Property> (value, _tiff_file);
-	}
+    template <typename Property>
+    bool get_property( typename Property::type& value  )
+    {
+      return get_property_f <mpl:: size <typename Property:: arg_types>::value > (). template operator () <Property> (value, _tiff_file);
+    }
 
     template <typename Property>
     inline
