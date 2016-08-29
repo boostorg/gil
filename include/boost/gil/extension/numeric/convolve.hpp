a2938936bf9a1472001a3a9382a9f73de9814e8c
Benjamin Buch <bebuch@users.noreply.github.com>
2016-08-29 18:23:59 +0200

    Fix includes in extension/numeric/convolve.hpp

--- include/boost/gil/extension/numeric/convolve.hpp
+++ include/boost/gil/extension/numeric/convolve.hpp
@@ -31,7 +31,8 @@
 #include <boost/gil/image_view_factory.hpp>
 #include <boost/gil/algorithm.hpp>
 #include <boost/gil/metafunctions.hpp>
-#include <pixel_numeric_operations.hpp>
+#include <boost/gil/extension/numeric/pixel_numeric_operations.hpp>
+#include <boost/gil/extension/numeric/algorithm.hpp>
 
 namespace boost { namespace gil {
 
