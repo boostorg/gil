65b56d1f5e88284a8ca17fed7f9bb5be9a32149e
Mateusz Łoskot <mateusz@loskot.net>
2018-12-15 04:08:20 +0100

    Apply clang-tidy modernize-use-using to rgb.hpp
    Apply clang-tidy modernize-use-using to rgba.hpp

--- include/boost/gil/rgb.hpp
+++ include/boost/gil/rgb.hpp
@@ -32,12 +32,12 @@ struct blue_t {};
 /// \}
 
 /// \ingroup ColorSpaceModel
-typedef mpl::vector3<red_t,green_t,blue_t> rgb_t;
+using rgb_t = mpl::vector3<red_t,green_t,blue_t>;
 
 /// \ingroup LayoutModel
-typedef layout<rgb_t> rgb_layout_t;
+using rgb_layout_t = layout<rgb_t>;
 /// \ingroup LayoutModel
-typedef layout<rgb_t, mpl::vector3_c<int,2,1,0> > bgr_layout_t;
+using bgr_layout_t = layout<rgb_t, mpl::vector3_c<int,2,1,0>>;
 
 /// \ingroup ImageViewConstructors
 /// \brief from raw RGB planar data
@@ -46,10 +46,11 @@ inline
 typename type_from_x_iterator<planar_pixel_iterator<IC,rgb_t> >::view_t
 planar_rgb_view(std::size_t width, std::size_t height,
                 IC r, IC g, IC b,
-                std::ptrdiff_t rowsize_in_bytes) {
-    typedef typename type_from_x_iterator<planar_pixel_iterator<IC,rgb_t> >::view_t RView;
-    return RView(width, height,
-                 typename RView::locator(planar_pixel_iterator<IC,rgb_t>(r,g,b),
+                std::ptrdiff_t rowsize_in_bytes)
+{
+    using view_t = typename type_from_x_iterator<planar_pixel_iterator<IC,rgb_t>>::view_t;
+    return view_t(width, height,
+                 typename view_t::locator(planar_pixel_iterator<IC,rgb_t>(r,g,b),
                                          rowsize_in_bytes));
 }
 
--- include/boost/gil/rgba.hpp
+++ include/boost/gil/rgba.hpp
@@ -23,16 +23,16 @@ namespace boost { namespace gil {
 struct alpha_t {};
 
 /// \ingroup ColorSpaceModel
-typedef mpl::vector4<red_t,green_t,blue_t,alpha_t> rgba_t;
+using rgba_t = mpl::vector4<red_t,green_t,blue_t,alpha_t>;
 
 /// \ingroup LayoutModel
-typedef layout<rgba_t> rgba_layout_t;
+using rgba_layout_t = layout<rgba_t>;
 /// \ingroup LayoutModel
-typedef layout<rgba_t, mpl::vector4_c<int,2,1,0,3> > bgra_layout_t;
+using bgra_layout_t = layout<rgba_t, mpl::vector4_c<int,2,1,0,3>>;
 /// \ingroup LayoutModel
-typedef layout<rgba_t, mpl::vector4_c<int,1,2,3,0> > argb_layout_t;
+using argb_layout_t = layout<rgba_t, mpl::vector4_c<int,1,2,3,0>>;
 /// \ingroup LayoutModel
-typedef layout<rgba_t, mpl::vector4_c<int,3,2,1,0> > abgr_layout_t;
+using abgr_layout_t = layout<rgba_t, mpl::vector4_c<int,3,2,1,0>>;
 
 /// \ingroup ImageViewConstructors
 /// \brief from raw RGBA planar data
@@ -43,9 +43,9 @@ planar_rgba_view(std::size_t width, std::size_t height,
                  IC r, IC g, IC b, IC a,
                  std::ptrdiff_t rowsize_in_bytes)
 {
-    typedef typename type_from_x_iterator<planar_pixel_iterator<IC,rgba_t> >::view_t RView;
+    using view_t = typename type_from_x_iterator<planar_pixel_iterator<IC,rgba_t> >::view_t;
     return RView(width, height,
-                 typename RView::locator(planar_pixel_iterator<IC,rgba_t>(r,g,b,a),
+                 typename view_t::locator(planar_pixel_iterator<IC,rgba_t>(r,g,b,a),
                                          rowsize_in_bytes));
 }
 
