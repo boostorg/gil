8170f030584b30b93f6f4cffeea63ffd844afb50
Mateusz Łoskot <mateusz@loskot.net>
2018-12-17 23:44:22 +0100

    Fix hard-wired use of Boost.Array static_size member
    Fixes #142

--- example/convolution.cpp
+++ example/convolution.cpp
@@ -6,7 +6,7 @@
 // http://www.boost.org/LICENSE_1_0.txt
 #include <boost/gil/image.hpp>
 #include <boost/gil/typedefs.hpp>
-#include <boost/gil/extension/io/jpeg_io.hpp>
+#include <boost/gil/extension/io/jpeg.hpp>
 #include <boost/gil/extension/numeric/kernel.hpp>
 #include <boost/gil/extension/numeric/convolve.hpp>
 
@@ -16,7 +16,7 @@ int main() {
     using namespace boost::gil;
 
     rgb8_image_t img;
-    jpeg_read_image("test.jpg",img);
+    read_image("test.jpg", img, jpeg_tag{});
 
     // Convolve the rows and the columns of the image with a fixed kernel
     rgb8_image_t convolved(img);
@@ -56,16 +56,15 @@ int main() {
     */
 
     kernel_1d_fixed<float,9> kernel(gaussian_1,4);
-
     convolve_rows_fixed<rgb32f_pixel_t>(const_view(convolved),kernel,view(convolved));
     convolve_cols_fixed<rgb32f_pixel_t>(const_view(convolved),kernel,view(convolved));
-    jpeg_write_view("out-convolution.jpg", view(convolved));
+    write_view("out-convolution.jpg", view(convolved), jpeg_tag{});
 
     // This is how to use a resizable kernel
     kernel_1d<float> kernel2(gaussian_1,9,4);
     convolve_rows<rgb32f_pixel_t>(const_view(img),kernel2,view(img));
     convolve_cols<rgb32f_pixel_t>(const_view(img),kernel2,view(img));
-    jpeg_write_view("out-convolution2.jpg", view(img));
+    write_view("out-convolution2.jpg", view(img), jpeg_tag{});
 
     return 0;
 }
