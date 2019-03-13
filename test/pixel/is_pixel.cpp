d3c1a2eff43540e1973977fcf524e47a4d92b1a7
Mateusz Łoskot <mateusz@loskot.net>
2019-03-13 19:15:33 +0100

    Add test for some bit-aligned pixel types and metafunctions (#257)
    Test bit_aligned_pixel_reference and packed_pixel as well as
    some of related metafunctions generating types of their
    members and intermediate specializations.

--- test/pixel/is_pixel.cpp
+++ test/pixel/is_pixel.cpp
@@ -5,9 +5,12 @@
 // See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt
 //
+#include <boost/gil/bit_aligned_pixel_reference.hpp>
+#include <boost/gil/packed_pixel.hpp>
 #include <boost/gil/pixel.hpp>
-#include <boost/gil/concepts/pixel.hpp>
+#include <boost/gil/rgb.hpp>
 #include <boost/gil/typedefs.hpp>
+#include <boost/gil/concepts/pixel.hpp>
 
 #include <boost/mp11.hpp>
 
@@ -40,7 +43,6 @@ int main()
         >::value,
         "is_pixel does not yield true for representative pixel types");
 
-
     static_assert(std::is_same
         <
             mp_all_of<gil::test::fixture::non_pixels, gil::is_pixel>,
@@ -54,4 +56,22 @@ int main()
             std::true_type
         >::value,
         "is_pixel yields true for non-pixel type");
+
+    using bgr121_ref_t = gil::bit_aligned_pixel_reference
+        <
+            std::uint8_t, boost::mpl::vector3_c<int, 1, 2, 1>, gil::bgr_layout_t, true
+        >;
+    static_assert(gil::is_pixel<bgr121_ref_t>::value,
+        "is_pixel does not yield true for bit_aligned_pixel_reference");
+
+    using rgb121_ref_t = gil::bit_aligned_pixel_reference
+        <
+            std::uint8_t, boost::mpl::vector3_c<int, 1, 2, 1>, gil::rgb_layout_t, true
+        >;
+    static_assert(gil::is_pixel<bgr121_ref_t>::value,
+        "is_pixel does not yield true for bit_aligned_pixel_reference");
+
+    using rgb121_packed_pixel_t = rgb121_ref_t::value_type;
+    static_assert(gil::is_pixel<rgb121_packed_pixel_t>::value,
+        "is_pixel does not yield true for packed_pixel");
 }
