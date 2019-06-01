7f75342fc1eeb2b793d847cfab311fa954afaa94
Mateusz Łoskot <mateusz@loskot.net>
2019-06-01 23:06:24 +0200

    Disable BOOST_GIL_USE_CONCEPT_CHECK when compiling tests with clang
    Avoid Clang flooding with non-disableable warnings like:
    
       T does not declare any constructor to initialize its non-modifiable members
    
    when compiling with concepts check enabled.
    
    Bug 41759: `warn_no_constructor_for_refconst` can not be disabled (Boost.ConceptCheck)
    https://bugs.llvm.org/show_bug.cgi?id=41759

--- test/core/pixel/concepts.cpp
+++ test/core/pixel/concepts.cpp
@@ -5,7 +5,11 @@
 // See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt
 //
-#ifndef BOOST_GIL_USE_CONCEPT_CHECK
+// FIXME: Avoid Clang's flooding of non-disableable warnings like:
+// "T does not declare any constructor to initialize its non-modifiable members"
+// when compiling with concepts check enabled.
+// See https://bugs.llvm.org/show_bug.cgi?id=41759
+#if !defined(BOOST_GIL_USE_CONCEPT_CHECK) && !defined(__clang__)
 #error Compile with BOOST_GIL_USE_CONCEPT_CHECK defined
 #endif
 #include <boost/gil.hpp>
