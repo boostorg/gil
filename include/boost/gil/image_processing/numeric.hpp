b2c154eeab1b1acb33be135bc258da3abee193a2
Mateusz Łoskot <mateusz@loskot.net>
2019-07-09 00:28:02 +0200

    Tidy up headers in gil/image_processing/ directory
    Add missing `inline` specifier for tiny functions.
    Add missing include guards.
    Add missing copyright notice.
    Remove superfluous `boost::gil::` namespace qualifiers.
    Make use of 100 characters line length limit:
     - Remove superfluous newlines to avoid too much of vertical stretch.
     - Tidy up Doxygen comments.

--- include/boost/gil/image_processing/numeric.hpp
+++ include/boost/gil/image_processing/numeric.hpp
@@ -1,20 +1,30 @@
+//
+// Copyright 2019 Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
+//
+// Use, modification and distribution are subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef BOOST_GIL_IMAGE_PROCESSING_NUMERIC_HPP
+#define BOOST_GIL_IMAGE_PROCESSING_NUMERIC_HPP
+
 #include <boost/gil/detail/math.hpp>
 #include <cmath>
 
-namespace boost{ namespace gil{
+namespace boost { namespace gil {
 
 /// \defgroup ImageProcessingMath
 /// \brief Math operations for IP algorithms
 ///
-/// This is mostly handful of mathemtical
-/// operations that are required by other
+/// This is mostly handful of mathemtical operations that are required by other
 /// image processing algorithms
-
+///
 /// \brief Normalized cardinal sine
 /// \ingroup ImageProcessingMath
 ///
 /// normalized_sinc(x) = sin(pi * x) / (pi * x)
-double normalized_sinc(double x)
+///
+inline double normalized_sinc(double x)
 {
     return std::sin(x * boost::gil::pi) / (x * boost::gil::pi);
 }
@@ -26,18 +36,17 @@ double normalized_sinc(double x)
 /// x == 0: 1
 /// -a < x && x < a: 0
 /// otherwise: normalized_sinc(x) / normalized_sinc(x / a)
-double lanczos(double x, std::ptrdiff_t a)
+inline double lanczos(double x, std::ptrdiff_t a)
 {
     if (x == 0)
-    {
         return 1;
-    }
+
     if (-a < x && x < a)
-    {
-        return normalized_sinc(x)
-               / normalized_sinc(x / static_cast<double>(a));
-    }
+        return normalized_sinc(x) / normalized_sinc(x / static_cast<double>(a));
 
     return 0;
 }
-}}
+
+}} // namespace boost::gil
+
+#endif
