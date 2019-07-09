266bf414aa4d43b789e912676fc7cced00aa0368
Mateusz Łoskot <mateusz@loskot.net>
2019-07-09 10:01:43 +0200

    Tidy up headers in gil/image_processing/ directory (#326)
    Add missing `inline` specifier for tiny functions.
    Add missing include guards.
    Add missing copyright notice.
    Remove superfluous `boost::gil::` namespace qualifiers.
    Make use of 100 characters line length limit:
     - Remove superfluous newlines to avoid too much of vertical stretch.
     - Tidy up Doxygen comments.

--- include/boost/gil/detail/math.hpp
+++ include/boost/gil/detail/math.hpp
@@ -1,3 +1,17 @@
-namespace boost{ namespace gil{
-    static constexpr double pi = 3.14159265358979323846;
-}}
+//
+// Copyright 2019 Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
+//
+// Use, modification and distribution are subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef BOOST_GIL_IMAGE_PROCESSING_DETAIL_MATH_HPP
+#define BOOST_GIL_IMAGE_PROCESSING_DETAIL_MATH_HPP
+
+namespace boost { namespace gil {
+
+static constexpr double pi = 3.14159265358979323846;
+
+}} // namespace boost::gil
+
+#endif
