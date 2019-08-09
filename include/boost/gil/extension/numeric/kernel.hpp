f6c502be02abbd9c89e666a8b905f539d6f456e7
MIRAL SHAH <37846212+miralshah365@users.noreply.github.com>
2019-08-09 21:06:37 +0530

    Fix Boost guidelines violations (#378)
    Add copyright notice.
    Add protection from the min/max macro.
    
    Closes #376

--- include/boost/gil/extension/numeric/kernel.hpp
+++ include/boost/gil/extension/numeric/kernel.hpp
@@ -1,10 +1,12 @@
 //
 // Copyright 2005-2007 Adobe Systems Incorporated
+// Copyright 2019 Miral Shah <miralshah2211@gmail.com>
 //
 // Distributed under the Boost Software License, Version 1.0
 // See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt
 //
+
 #ifndef BOOST_GIL_EXTENSION_NUMERIC_KERNEL_HPP
 #define BOOST_GIL_EXTENSION_NUMERIC_KERNEL_HPP
 
