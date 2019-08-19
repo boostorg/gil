5f612ccf7a9544c9c3193e64bf47f90bdb0fbcfd
MIRAL SHAH <37846212+miralshah365@users.noreply.github.com>
2019-08-20 02:04:11 +0530

    added definition of gaussian adaptive threshold (#379)
    closes #316

--- example/adaptive_threshold.cpp
+++ example/adaptive_threshold.cpp
@@ -7,6 +7,7 @@
 //
 #include <boost/gil.hpp>
 #include <boost/gil/extension/io/png.hpp>
+#include <iostream>
 
 using namespace boost::gil;
 
@@ -16,14 +17,17 @@ int main()
     read_image("test_adaptive.png", img, png_tag{});
     gray8_image_t img_out(img.dimensions());
 
-    //    performing binary threshold on each channel of the image
-    //    if the pixel value is more than 150 than it will be set to 255 else to 0
-    boost::gil::threshold_adaptive(const_view(img), view(img_out), 11);
-    write_view("out-threshold-adaptive.png", view(img_out), png_tag{});
+    boost::gil::threshold_adaptive(const_view(img), view(img_out), 11, threshold_adaptive_method::mean, threshold_direction::regular, 2);
+    write_view("out-threshold-adaptive-mean.png", view(img_out), png_tag{});
+    
+    boost::gil::threshold_adaptive(const_view(img), view(img_out), 11, threshold_adaptive_method::mean, threshold_direction::inverse, 2);
+    write_view("out-threshold-adaptive-mean-inv.png", view(img_out), png_tag{});
 
-    //    if the pixel value is more than 150 than it will be set to 150 else no change
-    boost::gil::threshold_adaptive(const_view(img), view(img_out), 11, threshold_adaptive_method::mean, threshold_direction::inverse);
-    write_view("out-threshold-adaptive-inv.png", view(img_out), png_tag{});
+    boost::gil::threshold_adaptive(const_view(img), view(img_out), 7, threshold_adaptive_method::gaussian, threshold_direction::regular, 2);
+    write_view("out-threshold-adaptive-gaussian.png", view(img_out), png_tag{});
+    
+    boost::gil::threshold_adaptive(const_view(img), view(img_out), 11, threshold_adaptive_method::gaussian, threshold_direction::inverse, 2);
+    write_view("out-threshold-adaptive-gaussian-inv.png", view(img_out), png_tag{});
 
     return 0;
 }
