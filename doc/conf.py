1f75e8530001d30a1cca6095574ed7e81ba7a734
Mateusz Łoskot <mateusz@loskot.net>
2019-09-03 20:21:40 +0200

    [doc] Remove incomplete copyright info from Sphinx config [ci skip]

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Boost.GIL'
-copyright = u'2018, Stefan Seefeld'
+copyright = u''
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
