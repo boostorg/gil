ad554e98339ddf3857a07d4bf858f023f1531ca3
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2019-10-08 12:13:15 +0600

    Fixed pixel16 used in image16s
    In Harris and Hessian tests, unsigned
    pixel values was used to construct
    signed image, which was causing
    appveyor to error out.

--- test/core/image_processing/hessian.cpp
+++ test/core/image_processing/hessian.cpp
@@ -34,8 +34,8 @@ bool are_equal(gil::gray32f_view_t expected, gil::gray32f_view_t actual) {
 void test_blank_image()
 {
     const gil::point_t dimensions(20, 20);
-    gil::gray16s_image_t dx(dimensions, gil::gray16_pixel_t(0), 0);
-    gil::gray16s_image_t dy(dimensions, gil::gray16_pixel_t(0), 0);
+    gil::gray16s_image_t dx(dimensions, gil::gray16s_pixel_t(0), 0);
+    gil::gray16s_image_t dy(dimensions, gil::gray16s_pixel_t(0), 0);
 
     gil::gray32f_image_t m11(dimensions);
     gil::gray32f_image_t m12_21(dimensions);
