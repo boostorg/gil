9b055f285b367e1b6cf704e5a4939e559b68d777
Samuel Debionne <samuel.debionne@gmail.com>
2020-04-06 23:50:01 +0200

    Add move semantics to image class (#457)
    Since we do not require C++17 yet with its `if constexpr
    that would discard instantiation of allocator's `operator=`
    when the statement is not active, we have to use the tag
    dispatching with appropriate used of
    `propagate_on_container_move_assignment` and
    rebound `allocator_type` to choose the POCMA.
    
    Minor coding style fixes.

--- test/core/image/image.cpp
+++ test/core/image/image.cpp
@@ -38,9 +38,54 @@ struct test_constructor_with_dimensions_pixel
     }
 };
 
+struct test_move_constructor
+{
+    template <typename Image>
+    void operator()(Image const&)
+    {
+        using image_t = Image;
+        gil::point_t const dimensions{256, 128};
+        {
+            image_t image(fixture::create_image<image_t>(dimensions.x, dimensions.y, 0));
+
+            image_t image2(std::move(image));
+            BOOST_TEST_EQ(image2.dimensions(), dimensions);
+            BOOST_TEST_EQ(image.dimensions(), gil::point_t{});
+        }
+    }
+    static void run()
+    {
+        boost::mp11::mp_for_each<fixture::image_types>(test_move_constructor{});
+    }
+};
+
+struct test_move_assignement
+{
+    template <typename Image>
+    void operator()(Image const&)
+    {
+        using image_t = Image;
+        gil::point_t const dimensions{256, 128};
+        {
+            image_t image = fixture::create_image<image_t>(dimensions.x, dimensions.y, 0);
+            image_t image2 = fixture::create_image<image_t>(dimensions.x * 2, dimensions.y * 2, 1);
+            image2 = std::move(image);
+            BOOST_TEST_EQ(image2.dimensions(), dimensions);
+            BOOST_TEST_EQ(image.dimensions(), gil::point_t{});
+        }
+    }
+    static void run()
+    {
+        boost::mp11::mp_for_each<fixture::image_types>(test_move_assignement{});
+    }
+};
+
 int main()
 {
     test_constructor_with_dimensions_pixel::run();
+
+    test_move_constructor::run();
+    test_move_assignement::run();
 
     return ::boost::report_errors();
 }
