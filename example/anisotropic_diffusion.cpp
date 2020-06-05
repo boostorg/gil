bb7d83a0b828aae95400e82238b3b96f946e7105
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2020-06-06 02:42:14 +0600

    Stylistical changes

--- example/anisotropic_diffusion.cpp
+++ example/anisotropic_diffusion.cpp
@@ -1,21 +1,30 @@
+//
+// Copyright 2020 Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
+//
+// Use, modification and distribution are subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
 #include <boost/gil/algorithm.hpp>
-#include <boost/gil/typedefs.hpp>
+#include <boost/gil/extension/io/png.hpp>
 #include <boost/gil/image.hpp>
 #include <boost/gil/image_view.hpp>
-#include <boost/gil/extension/io/png.hpp>
+#include <boost/gil/image_processing/diffusion.hpp>
 #include <boost/gil/io/typedefs.hpp>
-#include <boost/gil/image_processing/numeric.hpp>
+#include <boost/gil/typedefs.hpp>
 
 #include <cmath>
+#include <iostream>
 #include <string>
-#include <vector>
 #include <type_traits>
+#include <vector>
 
 namespace gil = boost::gil;
 
-#include <iostream>
-
-void gray_version(const std::string& input_path, const std::string& output_path, unsigned int iteration_count, unsigned int kappa)
+void gray_version(std::string const& input_path,
+    std::string const& output_path,
+    unsigned int iteration_count,
+    unsigned int kappa)
 {
     gil::gray8_image_t input;
     gil::read_image(input_path, input, gil::png_tag{});
@@ -43,7 +52,10 @@ void gray_version(const std::string& input_path, const std::string& output_path,
     gil::write_view(output_path, gil::view(true_output), gil::png_tag{});
 }
 
-void rgb_version(const std::string& input_path, const std::string& output_path, unsigned int iteration_count, unsigned int kappa)
+void rgb_version(const std::string& input_path,
+    const std::string& output_path,
+    unsigned int iteration_count,
+    unsigned int kappa)
 {
     gil::rgb8_image_t input;
     gil::read_image(input_path, input, gil::png_tag{});
@@ -70,14 +82,15 @@ void rgb_version(const std::string& input_path, const std::string& output_path, 
                 static_cast<gil::uint8_t>(p[2]));
     });
 
-    gil::write_view(output_path, gil::view(true_output), gil::png_tag{});    
+    gil::write_view(output_path, gil::view(true_output), gil::png_tag{});
 }
 
 int main(int argc, char* argv[])
 {
     if (argc != 6)
     {
-        std::cerr << "usage: " << argv[0] << " <gray input.png> <output.png> <colorspace: gray|rgb> <positive iteration count> <positive kappa~30>\n";
+        std::cerr << "usage: " << argv[0] << " <input.png> <output.png>"
+            " <colorspace: gray|rgb> <positive iteration count> <positive kappa~30>\n";
         return -1;
     }
     std::string input_path = argv[1];
@@ -85,14 +98,14 @@ int main(int argc, char* argv[])
     std::string colorspace = argv[3];
 
     unsigned int iteration_count = static_cast<unsigned int>(std::stoul(argv[4]));
-    unsigned int kappa = static_cast<unsigned int>(std::stoul(argv[5]));    
+    unsigned int kappa = static_cast<unsigned int>(std::stoul(argv[5]));
     if (colorspace == "gray")
     {
         gray_version(input_path, output_path, iteration_count, kappa);
     } else if (colorspace == "rgb")
     {
         rgb_version(input_path, output_path, iteration_count, kappa);
-    } else 
+    } else
     {
         std::cerr << "unknown colorspace option passed (did you type gray with A?)\n";
     }
