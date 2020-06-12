f043750487c8e41fe486799c4dec483a83ede70d
DEBABRATA MANDAL <debman@cse.iitb.ac.in>
2020-06-13 00:07:36 +0530

    Updates histogram class example file

--- example/histogram_class.cpp
+++ example/histogram_class.cpp
@@ -18,58 +18,25 @@
 using namespace boost::gil;
 
 int main() {
+    //Check key_from_pixel
     histogram<int,int,int> a,c;
+    int i = 0;
+    std::cout<<"Val: "<<(i = std::get<2>(a.key_from_pixel(rgb8_pixel_t(1))))<<"\n";
+
     histogram<int,int,std::string> e;
-    // std::cout<<rgb8_pixel_t(1);
-    // std::cout<<std::get<2>(a.key_from_pixel(rgb8_pixel_t(1)));
-    std::cout<<"\n"<<e.is_pixel_compatible();
-    histogram<int,int> b;
-    // gray8_image_t v(1,2,gray8_pixel_t(0));
-    // fill_histogram<1,2>(const_view(v),a);
-    histogram<int>::mapped_type d=12;
-    std::cout<<d;
-    // a(1) = 9;
+
+    gray8_image_t v1(1,2,gray8_pixel_t(0));
+    // fill_histogram<1,2>(const_view(v1),a);   //Fails
+
+    a(1, 1, 1) = 9;
+    histogram<int, int> b;
     b(1,2) = 1;
-    // c = a;
-    // std::cout<<a(1)<<a.size();
-    // using tt = typename ::key_type;
+    c = a;
+    std::cout<<"Val :"<<a(1, 1, 1)<<" "<<a.size()<<"\n";
+    
+    // Some std::tuple checks
     std::tuple<int> t;
     t = 1;
-    // int b = 
-    // std::cout<<(t=={1});
-    // std::cout<<t;
-    // std::tuple<int, int> e(1,2);
-    // boost::hash<std::tuple<int ,int>> h;
-    // std::cout<<h(e);
-
-    std::cout<<"New";
-    histogram<int> x;
-    std::cout<<x.get_dimension()<<"dim";
-    gray8_image_t v(1,2,gray8_pixel_t(1));
-    rgb8_image_t rv(1,2,rgb8_pixel_t(1, 1, 1));
-    const std::size_t k=0;
-    // x.fill<0>(const_view(v));
-    // x.fill(const_view(v));
-    // x.fill<0,2>(const_view(v));
-    fill_histogram<0>(const_view(rv),x);
-    std::vector<int> vv;
-    // fill_histogram(const_view(v), vv, 1);
-    std::cout<<x[1];
-
-    std::cout<<"New2";
-    rgb8_image_t vx(1,2,rgb8_pixel_t(1,0,2));
-    histogram<int, int, int> xx;
-    xx.fill<1,0,2>(const_view(vx));
-    xx.fill(const_view(vx));
-    std::cout<<"Val: "<<xx(0,1,2);
-    std::cout<<"Val: "<<xx(1,0,2);
-    std::cout<<"Val: "<<xx(1,0,1);
-    // rgb8_pixel_t r = rgb8_pixel_t(1,0,2);
-    // std::tuple<int,int,int> rt = detail::pixel_to_tuple(r, boost::mp11::index_sequence<1,0,2>{});
-    // // std::cout<<std::get<1>(rt);
-    // std::cout<<"\n"<<std::get<0>(rt)<<" "<<std::get<1>(rt)<<" "<<std::get<2>(rt)<<" "<<"endl";
-
-    // auto i = xx.key_from_pixel<1,0,2>(rgb8_pixel_t(1,0,2));
-    // std::cout<<"\n"<<std::get<0>(i)<<" "<<std::get<1>(i)<<" "<<std::get<2>(i)<<" "<<"endl";
-
+    // std::cout<<(t=={1});     // Fails
+    // std::cout<<t;            // Fails
 }
