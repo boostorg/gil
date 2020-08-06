fc0f56f6d3b09a2ba0ef418ad5671e5ce1392e78
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2020-08-06 20:09:19 +0600

    Leave only Bresenham line rasterization
    Naive and incremental algorithms were
    removed because they are supposed
    to produce the same results anyway.
    The reason for diverging results
    is inaccuracy of floating point numbers

--- include/boost/gil/rasterization/line.hpp
+++ include/boost/gil/rasterization/line.hpp
@@ -3,84 +3,9 @@
 #include <cstddef>
 
 namespace boost { namespace gil {
-inline std::ptrdiff_t estimate_point_count(std::ptrdiff_t width, std::ptrdiff_t height)
+inline constexpr std::ptrdiff_t estimate_point_count(std::ptrdiff_t width, std::ptrdiff_t height)
 {
     return width > height ? width : height;
-}
-
-template <typename RAIterator>
-void rasterize_horizontal_line(RAIterator first, RAIterator last)
-{
-    std::ptrdiff_t x = 0;
-    for (; first != last; ++first)
-    {
-        *first = point_t{x, 0};
-        ++x;
-    }
-}
-
-template <typename RAIterator>
-void rasterize_vertical_line(RAIterator first, RAIterator last)
-{
-    std::ptrdiff_t y = 0;
-    for (; first != last; ++first)
-    {
-        *first = point_t{0, y};
-        ++y;
-    }
-}
-
-template <typename RAIterator>
-void rasterize_line_naive(std::ptrdiff_t width, std::ptrdiff_t height, RAIterator output_first)
-{
-    const auto point_count = estimate_point_count(width, height);
-    // walk over longer axis, swap coordinates later
-    const bool needs_flip = point_count != width;
-    // m is guaranteed to be <= 1
-    const double m = [needs_flip, width, height]() {
-        if (width == 1 || height == 1)
-        {
-            return 0.0;
-        }
-
-        return needs_flip ? static_cast<double>(width) / height
-                          : static_cast<double>(height) / width;
-    }();
-    *output_first++ = point_t(0, 0);
-    for (std::ptrdiff_t x = 1; x < point_count; ++x)
-    {
-        double y = m * x;
-        auto discrete_y = std::llround(y);
-        auto next_point = needs_flip ? point_t{discrete_y, x} : point_t{x, discrete_y};
-        *output_first++ = next_point;
-    }
-}
-
-template <typename RAIterator>
-void rasterize_line_incr(std::ptrdiff_t width, std::ptrdiff_t height, RAIterator output_first)
-{
-    const auto point_count = estimate_point_count(width, height);
-    // walk over longer axis, swap coordinates later
-    const bool needs_flip = point_count != width;
-    // m is guaranteed to be <= 1
-    const double m = [needs_flip, width, height]() {
-        if (width == 1 || height == 1)
-        {
-            return 0.0;
-        }
-
-        return needs_flip ? static_cast<double>(width) / height
-                          : static_cast<double>(height) / width;
-    }();
-    double y = 0;
-    *output_first++ = point_t(0, 0);
-    for (std::ptrdiff_t x = 1; x < point_count; ++x)
-    {
-        y += m;
-        auto discrete_y = std::llround(y);
-        auto next_point = needs_flip ? point_t{discrete_y, x} : point_t{x, discrete_y};
-        *output_first++ = next_point;
-    }
 }
 
 template <typename RAIterator>
@@ -103,7 +28,7 @@ void rasterize_line_bresenham(std::ptrdiff_t width, std::ptrdiff_t height, RAIte
     for (std::ptrdiff_t x = 1; x < point_count; ++x)
     {
         error_term += m;
-        if (error_term > 0.5)
+        if (error_term >= 0.5)
         {
             --error_term;
             ++y;
