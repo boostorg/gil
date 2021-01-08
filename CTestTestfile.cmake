480b2f9292e36e2118375657788a5edd5925ac13
meshtag <prathameshtagore@gmail.com>
2021-01-08 09:33:50 +0530

    Added dilation and erosion and removed build files

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/prathamesh/gil
-# Build directory: /home/prathamesh/gil
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("example")
--- example/CMakeFiles/example_interleaved_ptr.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for example_interleaved_ptr.
-# This may be replaced when dependencies are built.
