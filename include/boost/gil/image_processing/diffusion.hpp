33ab3cfc1034b0a67526966b3c126faa912624d2
Pranam Lashkari <lpranam@collabora.com>
2021-03-01 04:46:59 +0530

    added missing header guards (#568)
    added missing header guards in
    <boost/gil/image_processing/diffusion.hpp>

--- include/boost/gil/image_processing/diffusion.hpp
+++ include/boost/gil/image_processing/diffusion.hpp
@@ -1,10 +1,15 @@
 //
 // Copyright 2020 Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
+// Copyright 2021 Pranam Lashkari <plashkari628@gmail.com>
 //
 // Use, modification and distribution are subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
+
+#ifndef BOOST_GIL_IMAGE_PROCESSING_DIFFUSION_HPP
+#define BOOST_GIL_IMAGE_PROCESSING_DIFFUSION_HPP
+
 #include "boost/gil/detail/math.hpp"
 #include <boost/gil/algorithm.hpp>
 #include <boost/gil/color_base_algorithm.hpp>
@@ -419,3 +424,5 @@ void anisotropic_diffusion(const InputView& input, const OutputView& output, uns
 }
 
 }} // namespace boost::gil
+
+#endif
