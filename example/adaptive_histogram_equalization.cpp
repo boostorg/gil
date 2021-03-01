2562e112419226301effa78e77c55107b7a17bf4
Pranam Lashkari <lpranam@collabora.com>
2021-03-01 16:47:45 +0530

    numeric extension move into core 1
    moved numeric/algorithm.hpp into core

--- example/adaptive_histogram_equalization.cpp
+++ example/adaptive_histogram_equalization.cpp
@@ -1,5 +1,6 @@
 //
 // Copyright 2020 Debabrata Mandal <mandaldebabrata123@gmail.com>
+// Copyright 2021 Pranam Lashkari <plashkari628@gmail.com>
 //
 // Use, modification and distribution are subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
@@ -8,7 +9,7 @@
 
 #include <boost/gil.hpp>
 #include <boost/gil/extension/io/png.hpp>
-#include <boost/gil/extension/numeric/algorithm.hpp>
+#include <boost/gil/algorithm.hpp>
 #include <boost/gil/image_processing/adaptive_histogram_equalization.hpp>
 
 using namespace boost::gil;
--- test/core/algorithm/extend_boundary.cpp
+++ test/core/algorithm/extend_boundary.cpp
@@ -1,13 +1,14 @@
 //
-// Copyright 2019 Pranam Lashkari <plashkari628@gmail.com>
+// Copyright 2019-2021 Pranam Lashkari <plashkari628@gmail.com>
 //
 // Distributed under the Boost Software License, Version 1.0
 // See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt
 //
 #include <boost/gil.hpp>
-#include <boost/gil/extension/numeric/algorithm.hpp>
 #include <boost/gil/extension/numeric/convolve.hpp>
+
+#include <boost/gil/algorithm.hpp>
 
 #include <boost/core/lightweight_test.hpp>
 
