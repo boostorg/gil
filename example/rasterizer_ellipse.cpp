81fdb4b29e405232da62cf8cb0d85340663e2b3f
meshtag <prathameshtagore@gmail.com>
2021-03-23 07:57:19 +0530

    Add ellipse rasterizer
    Improve header inclusion
    
    Improve comments
    
    Improve casting

--- example/rasterizer_ellipse.cpp
+++ example/rasterizer_ellipse.cpp
@@ -1,22 +1,43 @@
-#include <boost/gil/extension/io/png.hpp>
+//
+// Copyright 2021 Prathamesh Tagore <prathameshtagore@gmail.com>
+//
+// Use, modification and distribution are subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
 #include <boost/gil/extension/io/jpeg.hpp>
-#include <boost/gil/rasterization/ellipse.hpp>
-#include <iostream>
+#include <boost/gil.hpp>
 
 namespace gil = boost::gil;
 
 int main()
 {
-    gil::rgb8_image_t img;
-    gil::read_image("test.jpg", img, gil::jpeg_tag{});
-    
-    gil::draw_ellipse(view(img), {255, 255, 0}, {60, 40}, 5, 20);
-    gil::write_view("example_ellipse.jpg", view(img), gil::jpeg_tag{});
+    // Syntax for usage :- 
+    // auto rasterizer = gil::midpoint_elliptical_rasterizer{};
+    // rasterizer(img_view, colour, center, semi-axes_length);
+    // Where
+    // img_view : gil view of the image on which ellipse is to be drawn.
+    // colour : Vector containing channel intensity values for img_view. Number of colours 
+    // provided must be equal to the number of channels present in img_view.
+    // center : Array containing x co-ordinate and y co-ordinate of the center respectively.
+    // semi-axes_length : Array containing lengths of horizontal semi-axis and vertical semi-axis
+    // respectively.
 
-    const std::ptrdiff_t size = 256;
-    gil::gray8_image_t buffer_image(size, size);
-    auto buffer = gil::view(buffer_image);
+    gil::gray8_image_t gray_buffer_image(256, 256);
+    auto gray_elliptical_rasterizer = gil::midpoint_elliptical_rasterizer{};
+    gray_elliptical_rasterizer(view(gray_buffer_image), {128}, {128, 128}, {100, 50});
 
-    gil::draw_ellipse(buffer, {255, 0, 0}, {128, 128}, 100, 50);
-    gil::write_view("ellipse_buffer.png", buffer, gil::png_tag{});
+    gil::rgb8_image_t rgb_buffer_image(256, 256);
+    auto rgb_elliptical_rasterizer = gil::midpoint_elliptical_rasterizer{};
+    rgb_elliptical_rasterizer(view(rgb_buffer_image), {0, 0, 255}, {128, 128}, {50, 100});
+
+    gil::rgb8_image_t rgb_buffer_image_out_of_bound(256, 256);
+    auto rgb_elliptical_rasterizer_out_of_bound = gil::midpoint_elliptical_rasterizer{};
+    rgb_elliptical_rasterizer_out_of_bound(view(rgb_buffer_image_out_of_bound), {255, 0, 0},
+                                                                            {100, 100}, {125, 125});
+
+    gil::write_view("rasterized_ellipse_gray.jpg", view(gray_buffer_image), gil::jpeg_tag{});
+    gil::write_view("rasterized_ellipse_rgb.jpg", view(rgb_buffer_image), gil::jpeg_tag{});
+    gil::write_view("rasterized_ellipse_rgb_out_of_bound.jpg", view(rgb_buffer_image_out_of_bound),
+                                                                                  gil::jpeg_tag{});
 }
--- include/boost/gil/rasterization/ellipse.hpp
+++ include/boost/gil/rasterization/ellipse.hpp
@@ -1,36 +1,55 @@
+//
+// Copyright 2021 Prathamesh Tagore <prathameshtagore@gmail.com>
+//
+// Use, modification and distribution are subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
 #ifndef BOOST_GIL_RASTERIZATION_ELLIPSE_HPP
 #define BOOST_GIL_RASTERIZATION_ELLIPSE_HPP
 
-#include <cmath>
 #include <array>
 #include <vector>
 #include <iostream>
 
 namespace boost { namespace gil {
 
-template<typename View>
-void draw_ellipse_impl(View view, const std::array<unsigned int, 3> colour,
- const std::array<unsigned int, 2> center, const int horizontal_axis_length,
- const int vertical_axis_length)
+/// \defgroup EllipseRasterization
+/// \ingroup Rasterization
+/// \brief Ellipse rasterization algorithms.
+
+/// \ingroup EllipseRasterization
+/// \brief Performs ellipse rasterization using midpoint algorithm.
+struct midpoint_elliptical_rasterizer
 {
-    static unsigned int colour_index;
-    static std::vector<std::array<std::ptrdiff_t, 2>> trajectory_points;
-    if(trajectory_points.size() == 0)
+    /// \brief Returns a vector containing co-ordinates of first quadrant points which lie on
+    /// rasterizer trajectory of the ellipse.
+    /// \param semi_axes - Array containing half of lengths of horizontal and vertical axis respectively.
+    std::vector<std::array<std::ptrdiff_t, 2>> obtain_trajectory(
+                                                    const std::array<unsigned int, 2> semi_axes)
     {
+        // Citation : J. Van Aken, "An Efficient Ellipse-Drawing Algorithm" in IEEE Computer Graphics and Applications, vol. 4, no. 09, pp. 24-35, 1984.
+        // doi: 10.1109/MCG.1984.275994
+        // keywords: {null}
+        // url: https://doi.ieeecomputersociety.org/10.1109/MCG.1984.275994
+        std::vector<std::array<std::ptrdiff_t, 2>> trajectory_points;
+        std::ptrdiff_t x = semi_axes[0], y = 0;
         
-        std::ptrdiff_t x = horizontal_axis_length, y = 0;
-        long long int t1, t2, t3, t4, t5, t6, t7, t8, t9, d1, d2;
-        t1 = horizontal_axis_length * horizontal_axis_length, t2 = 2 * t1, t3 = 2 * t2;
-        t4 = vertical_axis_length * vertical_axis_length, t5 = 2 * t4, t6 = 2 * t5;
-        t7 = horizontal_axis_length * t5, t8 = 2 * t7, t9 = 0;
+        // Variables declared on the following line are temporary variables used for improving 
+        // performance since they help in converting all multiplicative operations inside the loop
+        // in to additive/subtractive operations.
+        long long int t1, t2, t3, t4, t5, t6, t7, t8, t9;
+        t1 = semi_axes[0] * semi_axes[0], t2 = 2 * t1, t3 = 2 * t2;
+        t4 = semi_axes[1] * semi_axes[1], t5 = 2 * t4, t6 = 2 * t5;
+        t7 = semi_axes[0] * t5, t8 = 2 * t7, t9 = 0;
+
+        // Following variables serve as decision parameters and help in choosing the right point
+        // to be included in rasterizer trajectory.
+        long long int d1, d2;
         d1 = t2 - t7 + t4 / 2, d2 = t1 / 2 - t8 + t5;
+        
         while(d2 < 0)
         {
-            std::cout << "I am here\n";
-            std::cout << x << "  " << y << "  x  and  y\n";
-            view(center[0] + x, center[1] + y) = view(center[0] - x, center[1] + y) = 
-            view(center[0] - x, center[1] - y) = view(center[0] + x, center[1] - y) = 
-            colour[colour_index];
             trajectory_points.push_back({x, y});
             y += 1;
             t9 += t3;
@@ -49,10 +68,6 @@ void draw_ellipse_impl(View view, const std::array<unsigned int, 3> colour,
         }
         while(x >= 0)
         {
-            std::cout << x << "  " << y << "  x  and  y\n";
-            view(center[0] + x, center[1] + y) = view(center[0] - x, center[1] + y) = 
-            view(center[0] - x, center[1] - y) = view(center[0] + x, center[1] - y) = 
-            colour[colour_index];
             trajectory_points.push_back({x, y});
             x -= 1;
             t8 -= t6;
@@ -67,33 +82,100 @@ void draw_ellipse_impl(View view, const std::array<unsigned int, 3> colour,
                 d2 += t5 - t8;
             }
         }
+        return trajectory_points;
     }
-    else
+
+    /// \brief Helps in drawing ellipse by filling desired colours at pixel co-ordinates returned 
+    /// by function 'obtain_trajectory'.
+    /// \param view - Gil view of image which will contain the elliptical curve after it is drawn.
+    /// \param colour - Constant vector specifying colour intensity values for all channels present
+    ///                 in 'view'.
+    /// \param center - Constant array specifying co-ordinates of center of ellipse to be drawn.
+    /// \param trajectory_points - Constant vector specifying pixel co-ordinates of points lying 
+    ///                            on rasterizer trajectory.
+    /// \tparam View - Type of input image view.
+    template<typename View>
+    void draw_curve(View view, const std::vector<unsigned int> colour,
+    const std::array<unsigned int, 2> center,
+    const std::vector<std::array<std::ptrdiff_t, 2>> trajectory_points)
     {
-        for (auto point : trajectory_points)
+        for(std::size_t i = 0, colour_index = 0;i < view.num_channels(); ++i, ++colour_index)
         {
-            view(center[0] + point[0], center[1] + point[1]) =
-            view(center[0] - point[0], center[1] + point[1]) = 
-            view(center[0] - point[0], center[1] - point[1]) = 
-            view(center[0] + point[0], center[1] - point[1]) = 
-            colour[colour_index];
+            for(std::array<std::ptrdiff_t, 2> point : trajectory_points)
+            {
+                std::array<std::ptrdiff_t, 4> co_ords = {center[0] + point[0],
+                center[0] - point[0], center[1] + point[1], center[1] - point[1]
+                };
+                bool validity[4] = {0};
+                if(co_ords[0] < view.width())
+                {
+                    validity[0] = 1;
+                }
+                if(co_ords[1] >= 0 && co_ords[1] < view.width())
+                {
+                    validity[1] = 1;
+                }
+                if(co_ords[2] < view.height())
+                {
+                    validity[2] = 1;
+                }
+                if(co_ords[3] >= 0 && co_ords[3] < view.height())
+                {
+                    validity[3] = 1;
+                }
+                if(validity[0] && validity[2])
+                {
+                    nth_channel_view(view, i)(co_ords[0], co_ords[2]) = colour[colour_index];
+                }
+                if(validity[1] && validity[2])
+                {
+                    nth_channel_view(view, i)(co_ords[1], co_ords[2]) = colour[colour_index];
+                }
+                if(validity[1] && validity[3])
+                {
+                    nth_channel_view(view, i)(co_ords[1], co_ords[3]) = colour[colour_index];
+                }
+                if(validity[0] && validity[3])
+                {
+                    nth_channel_view(view, i)(co_ords[0], co_ords[3]) = colour[colour_index];
+                }
+            }
         }
     }
-    ++colour_index;
-}
 
-template<typename View>
-void draw_ellipse(View view, const std::array<unsigned int, 3> colour,
- const std::array<unsigned int, 2> center, const int horizontal_axis_length,
- const int vertical_axis_length)
-{
-    // Include checks
-    for(std::size_t i = 0;i < view.num_channels(); ++i)
+    /// \brief Calls the function 'obtain_trajectory' and then passes obtained trajectory points
+    ///        in the function 'draw_curve' for drawing the desired ellipse.
+    /// \param view - Gil view of image which will contain the elliptical curve after it is drawn.
+    /// \param colour - Constant vector specifying colour intensity values for all channels present
+    ///                 in 'view'.
+    /// \param center - Array specifying co-ordinates of center of ellipse to be drawn.
+    /// \param semi_axes - Array containing lengths of semi-major axis and semi-minor axis respectively.                
+    /// \tparam View - Type of input image view.
+    template<typename View>
+    void operator()(View view, const std::vector<unsigned int> colour,
+            std::array<unsigned int, 2> center, const std::array<unsigned int, 2> semi_axes)
     {
-        draw_ellipse_impl(nth_channel_view(view, i), colour, center, 2 * horizontal_axis_length ,
-                                                                         2 * vertical_axis_length);
+        --center[0], --center[1]; // For converting center co-ordinate values to zero based indexing.
+        if(colour.size() != view.num_channels())
+        {
+            throw std::length_error("Number of channels in given image is not equal to the "
+                                                                "number of colours provided.");
+        }
+        if(center[0] + semi_axes[0] >= view.width() || center[1] + semi_axes[1] >= view.height()
+            || static_cast<int>(center[0] - semi_axes[0]) < 0 
+            || static_cast<int>(center[0] - semi_axes[0]) >= view.width()
+            || static_cast<int>(center[1] - semi_axes[1]) < 0
+            || static_cast<int>(center[1] - semi_axes[1]) >= view.height())
+        {
+            std::cout << "Image can't contain whole curve.\n"
+            "However, it will contain those parts of curve which can fit inside it.\n";
+            std::cout << "Note : Image width = " << view.width() << " and Image height = "
+                                                                          << view.height() << "\n";
+        }
+        std::vector<std::array<std::ptrdiff_t, 2>> trajectory_points = 
+                                                              obtain_trajectory(semi_axes);
+        draw_curve(view, colour, center, trajectory_points);
     }
-}
-
+}; // midpoint elliptical rasterizer
 }} // namespace boost::gil
 #endif
