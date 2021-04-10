06ea1ff7fa7d001ecace0631ef67c46ecaa27980
meshtag <prathameshtagore@gmail.com>
2021-04-10 09:46:39 +0530

    Modified as per first review and compiler warnings suppression

--- include/boost/gil/extension/toolbox/metafunctions/shrink.hpp
+++ include/boost/gil/extension/toolbox/metafunctions/shrink.hpp
@@ -2,8 +2,8 @@
 // Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef _shrink_hpp_
-#define _shrink_hpp_
+#ifndef BOOST_GIL_EXTENSION_TOOLBOX_METAFUNCTIONS_SHRINK_HPP
+#define BOOST_GIL_EXTENSION_TOOLBOX_METAFUNCTIONS_SHRINK_HPP
 
 #include <boost/gil.hpp>
 
@@ -12,7 +12,7 @@ namespace boost { namespace gil {
 /// \brief Assigns a smaller subimage view of the view given as first parameter to the
 /// destination/resultant view given as second parameter to the overloaded '()' operator.
 /// The dimensions of the smaller view are specified by struct constructor parameters.
-template <typename view_t>
+template <typename View>
 struct shrink
 {
     double left;
@@ -20,27 +20,30 @@ struct shrink
     double right;
     double bottom;
 
-    shrink(double left = 5, double top = 5, double right = 5, double bottom = 5)
-        : left(left)
-        , top(top)
-        , right(right)
-        , bottom(bottom)
+    shrink(double left_param = 5, double top_param = 5, double right_param = 5,
+        double bottom_param = 5)
+        : left(left_param)
+        , top(top_param)
+        , right(right_param)
+        , bottom(bottom_param)
     {
     }
-
-    void operator()(view_t& view, view_t& dstView)
+ 
+    void operator()(View& view, View& dstView)
     {
+        double const view_width = static_cast<double>(view.width());
+        double const view_height = static_cast<double>(view.height());
         if (left < 1.0)
-            left *= view.width();
+            left *= view_width;
         if (right < 1.0)
-            right *= view.width();
+            right *= view_width;
         if (top < 1.0)
-            top *= view.height();
+            top *= view_height;
         if (bottom < 1.0)
-            bottom *= view.height();
+            bottom *= view_height;
 
-        double width = view.width() - left - right;
-        double height = view.height() - top - bottom;
+        double const width = view_width - left - right;
+        double const height = view_height - top - bottom;
         dstView = boost::gil::subimage_view(view, static_cast<std::ptrdiff_t>(left),
             static_cast<std::ptrdiff_t>(top), static_cast<std::ptrdiff_t>(width),
             static_cast<std::ptrdiff_t>(height));
