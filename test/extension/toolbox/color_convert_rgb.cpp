0f435906eade0e0d936bc07260da7fe8e278ff21
Mateusz Łoskot <mateusz@loskot.net>
2022-06-25 17:51:01 +0100

    test: Add tests for RGB to HSL (#691)
    Contributor of PR #505 posted GTest-based tests in.
    This commit ports those tests to Boost.LightweightTest.
    
    Closes #690

--- test/extension/toolbox/color_convert_rgb.cpp
+++ test/extension/toolbox/color_convert_rgb.cpp
@@ -13,11 +13,12 @@
 
 #include <cstdint>
 #include <tuple>
+#include <vector>
 
+#include "test_utility_output_stream.hpp"
 #include "test_utility_with_tolerance.hpp"
 
 namespace gil = boost::gil;
-
 
 void test_basic_rgb_to_hsl()
 {
@@ -28,6 +29,50 @@ void test_basic_rgb_to_hsl()
     BOOST_TEST_GT(gil::get_color(h, gil::hsl_color_space::hue_t()), 0.8);         // 0.83333331
     BOOST_TEST_EQ(gil::get_color(h, gil::hsl_color_space::saturation_t()), 1.0);  // 1.00000000
     BOOST_TEST_GT(gil::get_color(h, gil::hsl_color_space::lightness_t()), 0.25);  // 0.25098040
+}
+
+void test_colors_rgb_to_hsl()
+{
+    using color_t = std::tuple<std::uint8_t, std::uint8_t, std::uint8_t, float, float, float>;
+    std::vector<color_t> colors = {
+        {0, 0, 0, 0, 0, 0},               // black
+        {255, 255, 255, 0, 0, 1},         // white
+        {255, 0, 0, 0, 1, 0.5},           // red
+        {0, 255, 0, 2 / 6.f, 1, 0.5},     // lime
+        {0, 0, 255, 4 / 6.f, 1, 0.5},     // blue
+        {255, 255, 0, 1 / 6.f, 1, 0.5},   // yellow
+        {0, 255, 255, 3 / 6.f, 1, 0.5},   // cyan
+        {255, 0, 255, 5 / 6.f, 1, 0.5},   // magenta
+        {191, 191, 191, 0, 0, 0.75},      // silver
+        {128, 128, 128, 0, 0, 0.5},       // gray
+        {128, 0, 0, 0, 1, 0.25},          // maroon
+        {128, 128, 0, 1 / 6.f, 1, 0.25},  // olive
+        {0, 128, 0, 2 / 6.f, 1, 0.25},    // green
+        {128, 0, 128, 5 / 6.f, 1, 0.25},  // purple
+        {0, 128, 128, 3 / 6.f, 1, 0.25},  // teal
+        {0, 0, 128, 4 / 6.f, 1, 0.25},    // navy
+    };
+
+    for (auto&& color : colors)
+    {
+        std::uint8_t r{0}, g{0}, b{0};
+        float h{0}, s{0}, l{0};
+        std::tie(r, g, b, h, s, l) = color;
+        gil::rgb8_pixel_t rgb(r, g, b);
+        gil::hsl32f_pixel_t hsl;
+        gil::color_convert(rgb, hsl);
+
+        float const abs_error = 0.002;
+        BOOST_TEST_WITH(
+            h, get_color(hsl, gil::hsl_color_space::hue_t()),
+            gil::test::utility::with_tolerance<double>(abs_error));
+        BOOST_TEST_WITH(
+            s, get_color(hsl, gil::hsl_color_space::saturation_t()),
+            gil::test::utility::with_tolerance<double>(abs_error));
+        BOOST_TEST_WITH(
+            l, get_color(hsl, gil::hsl_color_space::lightness_t()),
+            gil::test::utility::with_tolerance<double>(abs_error));
+    }
 }
 
 void test_copy_pixels_rgb_to_hsl()
@@ -50,6 +95,7 @@ void test_copy_pixels_rgb_to_hsl()
 int main()
 {
     test_basic_rgb_to_hsl();
+    test_colors_rgb_to_hsl();
     test_copy_pixels_rgb_to_hsl();
 
     return ::boost::report_errors();
