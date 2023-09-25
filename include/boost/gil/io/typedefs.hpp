ca531c16837225b355aa2171a88e422529c1286f
Samuel Debionne <samuel.debionne@gmail.com>
2023-09-25 16:57:04 +0200

    fix: specialize std::is_floating_point (not using Boost.TypeTraits anymore)

--- include/boost/gil/io/typedefs.hpp
+++ include/boost/gil/io/typedefs.hpp
@@ -29,12 +29,12 @@ using byte_vector_t = std::vector<byte_t>;
 
 }} // namespace boost::gil
 
-namespace boost {
+namespace std {
 
-template<> struct is_floating_point<gil::float32_t> : std::true_type {};
-template<> struct is_floating_point<gil::float64_t> : std::true_type {};
+template<> struct is_floating_point<boost::gil::float32_t> : std::true_type {};
+template<> struct is_floating_point<boost::gil::float64_t> : std::true_type {};
 
-} // namespace boost
+} // namespace std
 
 namespace boost { namespace gil {
 
