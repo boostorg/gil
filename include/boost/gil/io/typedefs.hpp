dc07bcadcd7183172c0cbaa98d59c501b050f479
Samuel Debionne <samuel.debionne@gmail.com>
2025-04-04 16:33:23 +0200

    fix is_floating_point: symbol is not a class template

--- include/boost/gil/io/typedefs.hpp
+++ include/boost/gil/io/typedefs.hpp
@@ -29,7 +29,7 @@ using byte_vector_t = std::vector<byte_t>;
 
 }} // namespace boost::gil
 
-namespace boost {
+namespace std {
 
 template<> struct is_floating_point<gil::float32_t> : std::true_type {};
 template<> struct is_floating_point<gil::float64_t> : std::true_type {};
